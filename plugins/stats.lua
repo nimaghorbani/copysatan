bf97ca7aa7d227054284b96b006d5914a6164fd5
nimaghorbani <ni.gh0098@yahoo.com>
2015-12-21 02:45:34 -0800

    Update stats.lua

--- plugins/stats.lua
+++ plugins/stats.lua
@@ -91,7 +91,7 @@ local function bot_stats()
   return text
 end
 local function run(msg, matches)
-  if matches[1]:lower() == 'satan' then -- Put everything you like :)
+  if matches[1]:lower() == 'ghbot' then -- Put everything you like :)
     local about = _config.about_text
     local name = user_print_name(msg.from)
     savelog(msg.to.id, name.." ["..msg.from.id.."] used /satan ")
@@ -120,7 +120,7 @@ local function run(msg, matches)
         return
       end
     end
-    if matches[2] == "satan" then -- Put everything you like :)
+    if matches[2] == "ghbot" then -- Put everything you like :)
       if not is_admin(msg) then
         return "For admins only !"
       else
@@ -141,13 +141,13 @@ return {
     "^[!/$&]([Ss]tats)$",
     "^[!/$&]([Ss]tatslist)$",
     "^[!/$&]([Ss]tats) (group) (%d+)",
-    "^[!/$&]([Ss]tats) (satan)",
+    "^[!/$&]([Ss]tats) (gh)",
     "^([Ss]atan)",
     "^([Ss]tats)$",
     "^([Ss]tatslist)$",
     "^([Ss]tats) (group) (%d+)",
-    "^([Ss]tats) (satan)",
-    "^([Ss]atan)"
+    "^([Ss]tats) (gh)",
+    "^([Gg]hbot)"
     }, 
   run = run
 }
